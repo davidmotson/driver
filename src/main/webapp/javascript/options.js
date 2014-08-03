75dec53a26a972367270433d6c780dde282a1ddf
Noah Goetz - Local <nsgoetz@gmail.com>
2014-08-03 08:59:08 -0700

    added files

---
+++ src/main/webapp/javascript/options.js
@@ -0,0 +1,60 @@
+
+var filteredOptions;
+
+
+var newOption = function(price, time, id, type, subtype) {
+  d = {
+    price: price,
+    time: time,
+    id: id,
+    type: type, 
+    subtype: subtype,
+  }  
+  return d;
+}
+
+function sortPriceHelper(op1, op2){
+  return (op1["price"] - op2["price"]);
+}
+
+function sortByPrice(){
+  $("#options-table").empty();
+  sorted = filteredOptions.sort(sortPriceHelper);
+  l = sorted.length;
+  for (i = 0; i<l; i++){
+    addDOMOption(sorted[i]);
+  } 
+}
+
+function sortTimeHelper(op1, op2){
+  return (op1["time"] - op2["time"]);
+}
+
+function sortByTime(){
+  $("#options-table").empty();
+  sorted = filteredOptions.sort(sortTimeHelper);
+  l = sorted.length;
+  for (i = 0; i < l; i++){
+    addDOMOption(sorted[i]);
+  } 
+}
+
+function secondToMin(seconds){
+  return seconds/60;
+}
+
+function addDOMOption(option){
+  e = $("#tempListItem").clone();
+  e.attr("id", "");
+  e.find(".type").text(option["type"] + ":");
+  e.find(".subtype").text(option["subtype"]);
+  e.find(".time").text(option["time"] + " min");
+  e.find(".price").text("$" + option["price"]);
+  el = $("#options-list").append(e);
+  e.show();
+}
+
+$(document).ready(function(){
+  test = newOption(5, 4, 3, "Lyft", "Car")
+  addDOMOption(test);
+})
