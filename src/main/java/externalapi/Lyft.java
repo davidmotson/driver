82372b9d61b518297893ceef7c2599aedc34d77a
davidm <davidmotson@gmail.com>
2014-08-02 18:33:45 -0700

    logins for lyft, flywheel and sidecar

---
+++ src/main/java/externalapi/Lyft.java
@@ -0,0 +1,54 @@
+package externalapi;
+
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+
+import org.json.JSONObject;
+
+
+public class Lyft {
+	
+	public boolean success;
+	public JSONObject self;
+	
+	public Lyft(boolean b){
+		success = b;
+	}
+	
+	public Lyft(JSONObject self){
+		this.self = self;
+		success = true;
+	}
+	
+	public static void sendConfirmationCode(String phoneNumber){
+		JSONObject post = new JSONObject();
+		JSONObject phone = new JSONObject();
+		phone.put("number", phoneNumber);
+		post.put("phone", phone);
+		
+		ClientBuilder.newClient()
+		.target("https://api.lyft.com/phoneauth")
+		.request().post(Entity.json(post.toString()))
+		.getEntity();
+	}
+	
+	public static Lyft login(String phoneNumber, int code){
+		JSONObject post = new JSONObject();
+		JSONObject phone = new JSONObject();
+		phone.put("number", phoneNumber);
+		phone.put("verificationCode", code);
+		post.put("phone", phone);
+		
+		JSONObject output = new JSONObject(ClientBuilder.newClient()
+			.target("https://api.lyft.com/users")
+			.request().post(Entity.json(post.toString()))
+			.readEntity(String.class));
+		
+		if(output.has("errors")){
+			return new Lyft(false);
+		}else{
+			return new Lyft(output.getJSONObject("user"));
+		}
+	}
+
+}
