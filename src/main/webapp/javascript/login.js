7bb6108779b91fd5f03631d829db144c8d9e5515
Noah Goetz - Local <nsgoetz@gmail.com>
2014-08-03 08:36:08 -0700

    option js wip

--- src/main/webapp/javascript/login.js
+++ src/main/webapp/javascript/login.js
@@ -30,7 +30,8 @@ $(document).ready(function(){
   function validateEmail(email) { 
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
-  } 
+  }
+   
   function invalidPassword(password){
     if (password.length < 6 || password == "Password") {
       return "Password must be 6 or more charecters!"
