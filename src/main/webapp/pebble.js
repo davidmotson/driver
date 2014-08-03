6fc255b040a52d174e5ca03f76bc0af1a4f70d76
davidm <davidmotson@gmail.com>
2014-08-03 09:02:56 -0700

    pebble test

--- src/main/webapp/pebble.js
+++ src/main/webapp/pebble.js
@@ -1,1 +1,21 @@
-simply.title("Hello Bitches");
+var username = "davidmotson@gmail.com";
+var password = "password2";
+var token;
+var cars;
+var summoned;
+
+simply.fullscreen(true);
+simply.text({
+	title: "Driver",
+	subtitle: "Welcome to Driver",
+	body: "Logging in..."
+});
+
+simply.ajax({
+	method: "post",
+	url: "http://107.150.8.38:8080/driver/api/login",
+	type: "json",
+	data: {username: username, password: password},
+}, function(result){
+	simply.body(result);
+});
