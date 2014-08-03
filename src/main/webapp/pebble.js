a017d1cd74d28bb5d940719dfa56db2bdddd5bb4
davidm <davidmotson@gmail.com>
2014-08-03 10:11:31 -0700

    go go power rangers

--- src/main/webapp/pebble.js
+++ src/main/webapp/pebble.js
@@ -13,16 +13,11 @@ simply.text({
 ajax({
 	method: 'post',
 	url: 'http://107.150.8.38:8080/driver/api/login',
-	type: 'json',
-	data: JSON.stringify({username: username, password: password}),
+	data: "{username:" + username+", password:"+ password+"}",
 	async: true
 }, function(result){
 	simply.body(result)
 },function(result){
 	simply.body(result);
 	simply.title("error");
-});
-
-ajax({ url: 'http://xkcd.com/614/info.0.json', type: 'json' }, function(data){
-	simply.body(data);
 });
